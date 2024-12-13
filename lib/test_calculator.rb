1d2f4b0c085d1b7d6a1bb9fccb69a42315fa85a2
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2024-12-14 00:02:49 +0530

    Success for adding two number with two number

--- lib/test_calculator.rb
+++ lib/test_calculator.rb
@@ -2,9 +2,12 @@ module TestCalculator
 
   def add
     return 0 if empty?
-    num = split(',').map(&:to_i)
     return num[0] + num[1] if include?(',')
 
     to_i
+  end
+
+  def num
+    split(',').map(&:to_i)
   end
 end
