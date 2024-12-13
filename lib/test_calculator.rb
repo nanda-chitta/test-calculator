ccf9584ea456b50745df8250329ac03f9e61d7b9
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2024-12-13 23:59:37 +0530

    Success for adding two number with two digit number

--- lib/test_calculator.rb
+++ lib/test_calculator.rb
@@ -2,7 +2,8 @@ module TestCalculator
 
   def add
     return 0 if empty?
-    return self[0].to_i + self[2].to_i if include?(',')
+    num = split(',')
+    return num[0].to_i + num[1].to_i if include?(',')
 
     to_i
   end
