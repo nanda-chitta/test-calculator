3c99e14d58de118771e9d8f55822bf03307bbedd
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2024-12-14 00:56:45 +0530

    succes adding with 3 digit number

--- lib/test_calculator.rb
+++ lib/test_calculator.rb
@@ -2,7 +2,7 @@ module TestCalculator
 
   def add
     return 0 if empty?
-    return 38 if size > 4
+    return num.reduce { |s, n| s + n } if size > 4
     return num[0] + num[1] if include?(',')
 
     to_i
